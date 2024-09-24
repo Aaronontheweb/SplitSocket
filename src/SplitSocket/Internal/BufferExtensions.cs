6a0688050178be6c84f93ad1a38354ffaa60e1e2
Aaron Stannard <aaron@petabridge.com>
2024-09-24 16:05:48 -0500

    more infra

---
+++ src/SplitSocket/Internal/BufferExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SplitSocket.Internal;
+
+internal static class BufferExtensions
+{
+    public static ArraySegment<byte> GetArray(this Memory<byte> memory)
+    {
+        return ((ReadOnlyMemory<byte>)memory).GetArray();
+    }
+
+    public static ArraySegment<byte> GetArray(this ReadOnlyMemory<byte> memory)
+    {
+        if (!MemoryMarshal.TryGetArray(memory, out var result))
+        {
+            throw new InvalidOperationException("Buffer backed by array was expected");
+        }
+        return result;
+    }
+}
---
+++ src/SplitSocket/Internal/SocketOperationResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+
+namespace SplitSocket.Internal;
+
+internal readonly struct SocketOperationResult
+{
+    public readonly SocketException? SocketError;
+
+    public readonly int BytesTransferred;
+
+    [MemberNotNullWhen(true, nameof(SocketError))]
+    public readonly bool HasError => SocketError != null;
+
+    public SocketOperationResult(int bytesTransferred)
+    {
+        SocketError = null;
+        BytesTransferred = bytesTransferred;
+    }
+
+    public SocketOperationResult(SocketException exception)
+    {
+        SocketError = exception;
+        BytesTransferred = 0;
+    }
+}
